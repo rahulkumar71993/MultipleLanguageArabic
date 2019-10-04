c0938919c8f47ac4514b85d0601b8eaf42bd0817
rahul <rahulkumar71993@gmail.com>
2019-10-04 19:14:57 +0530

    dialog

--- app/src/main/java/com/example/multiplelanguage/MainActivity.java
+++ app/src/main/java/com/example/multiplelanguage/MainActivity.java
@@ -51,6 +51,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setLocale(PrefManager.getLanguage(MainActivity.this));
     }
 
     @Override
